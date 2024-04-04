9135d05c9077e23e2d0276cc86ae91c4ca48795b
himangshu <himangshu.jain01@gmail.com>
2024-04-04 07:32:36 +0530

    initial commit

--- src/connections/mongoDb.js
+++ src/connections/mongoDb.js
@@ -4,4 +4,5 @@ mongoose.connect(`${process.env.DB_URL}`).then(()=>{
     console.log("DB connected Successfully");
 }).catch((err)=>{
 console.log("error in connecting DB",err);
+console.log("error");
 })
